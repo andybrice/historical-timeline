3cc0a6f454d89d18193ec805fdddd85d43955d54
Andy Brice <andybrice@andybrice.config>
2013-10-24 15:16:34 +0100

    Removed list route Renamed debug route Added YAML parsing to timeline route Extended Builder class with checkmark method

--- historical-timeline.rb
+++ historical-timeline.rb
@@ -2,18 +2,20 @@ require 'sinatra'
 require 'yaml'
 require 'builder'
 
+class Builder::XmlMarkup
+  def check_mark( x, length, attributes = {} )
+    self.line attributes, :x1 => x, :y1 => 0, :x2 => x, :y2 => length
+  end
+end
+
 get '/timeline' do
+  @events = YAML.load_file('public/dates.yml')
   @year_range = -4000..2000
   @origin = { :x => 200.5, :y => 100.5 }
   builder :timeline
 end
 
-get '/data' do
+get '/debug-data' do
   @events = YAML.load_file('dates.yml')
   @events.inspect
-end
-
-get '/list' do
-  @events = YAML.load_file('dates.yml')
-  erb :list
 end
